36c35bbd929ff70f9a35eb75e357c691ca1487dd
Svenja Uslu <svenjauslu@hotmail.de>
2019-04-09 10:40:26 +0200

    [Svenja][#1.1] Added Welcome message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,42 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    //Streams for testing the command line outputs:
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        //Given
+        //When
+        BibliotecaApp.main(new String[1]);
+        //Then
+        assertEquals( "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n",
+                outContent.toString());
     }
 }
