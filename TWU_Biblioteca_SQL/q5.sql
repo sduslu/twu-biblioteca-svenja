212a97d5193594dc67f8e02e63f0acddbdba1322
Svenja Uslu <svenjauslu@hotmail.de>
2019-04-16 15:14:54 +0200

    [Svenja][SQL Assignment] Solved Q5 with Christins help

--- TWU_Biblioteca_SQL/q5.sql
+++ TWU_Biblioteca_SQL/q5.sql
@@ -1,4 +1,6 @@
-SELECT COUNT(checkout_item.book_id), COUNT(checkout_item.movie_id), checkout_item.member_id, member.name FROM checkout_item, member
+SELECT count_books, count_movies, name FROM (SELECT COUNT(checkout_item.book_id) AS count_books, COUNT(checkout_item.movie_id) AS count_movies, checkout_item.member_id, member.name FROM checkout_item, member
 WHERE member.id = checkout_item.member_id
 GROUP BY checkout_item.member_id
+) WHERE (count_books > 1 OR count_movies > 1)
+OR (count_books = 1 AND count_movies = 1)
 ;
